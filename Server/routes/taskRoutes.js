bd037b2a312e47733425f58f4544f02f445bf344
Alice Smith <alice@webage.com>
2025-03-19 15:32:48 -0400

    merged React and Server

--- Server/routes/taskRoutes.js
+++ Server/routes/taskRoutes.js
@@ -5,7 +5,7 @@ const taskController = require("../controllers/taskController");
  
 router.get("/tasks", taskController.getAllTasks); 
 router.get("/tasks/:id", taskController.getTaskById); 
-router.post("/create-task", taskController.createTask); 
+router.post("/tasks/create-task", taskController.createTask); 
 router.delete("/tasks/:id/delete", taskController.deleteTaskById); 
 router.patch("/tasks/:id/status/update", taskController.updateTaskStatus);
 
